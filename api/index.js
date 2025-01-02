9b35afbdd47018d78033a0b40d53d777a527eb02
yokoyoko7 <tiffanyliym@gmail.com>
2025-01-02 18:10:26 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://digital-menu-sandy.vercel.app",
+    origin: "https://digital-menu-1dist.onrender.com",
   })
 );
 app.use(express.json());
